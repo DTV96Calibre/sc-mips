d08a10f183e80ed0eb9a00d8064fba60cea95761
Daniel Vasquez <dtv002@bucknell.edu>
2017-09-07 15:06:31 -0400

    Finished control module

--- processor.v
+++ processor.v
@@ -46,19 +46,52 @@ module mux2_1 (input high_a, input [31:0] a, input [31:0] b, output [31:0] out);
 endmodule
 
 // -------------CONTROL-----------------------------------
-module control (input [31:26] opcode, output reg regdst,jump,branch,memread,memtoreg,output reg [2:0] aluop, output reg memwrite,alusrc,regwrite);
+module control (input [31:26] opcode, output reg regdst,jump,branch,memread,memtoreg,output reg [3:0] aluop, output reg memwrite,alusrc,regwrite,invertzero);
   always @(opcode) begin
-    // Regdst
+    // Regdst (there are fewer 0s than 1s so checked for 0s)--------VVVVVVV
+    regdst <= (opcode == `ADDI || opcode == `ORI || opcode == `LW) ? 0 : 1;
 
     // Check or jump instructions
     jump <= (opcode == `J||opcode == `JAL) ? 1 : 0;
 
+    // branch
+    branch <= (opcode ==`BEQ||opcode == `BNE) ? 1 : 0;
+
+    // memread
+    memread <= (opcode ==`LW) ? 1 : 0;
+
+    // memtoreg
+    memtoreg <= (opcode == `LW) ? 1 : 0;
+
+    // aluop
+    case(opcode)
+      `AND: aluop = `ALU_AND;
+      `OR: aluop = `ALU_OR;
+      `ORI: aluop = `ALU_OR;
+      `ADD: aluop = `ALU_add;
+      `ADDI: aluop = `ALU_add;
+      `LW: aluop = `ALU_add;
+      `SW: aluop = `ALU_add;
+      `SUB: aluop = `ALU_sub;
+      `BEQ: aluop = `ALU_sub;
+      `BNE: aluop = `ALU_sub;
+      `SLT: aluop = `ALU_slt;
+      default: aluop = `ALU_undef;
+    endcase;
+
+    // memwrite
+    memwrite <= (opcode ==`SW) ? 1 : 0;
+
+    // alusrc
+    alusrc <= (opcode==`ADDI||opcode==`ORI||opcode==`LW||opcode==`SW)?1:0;
+
     // Only sw and beq/bne don't write to a reg (regdst=x, regwrite=0, memtoreg=x)
     regdst <= (opcode == `SW||opcode == `BEQ||opcode == `BNE) ? 0 : 1;
 
-    // Only sw and lw use aluop
+    // Invert ALU Zero signal?
+    // Subtract OP results in Zero=1 when ==, so flip Zero when checking !=
+    invertzero <= (opcode == `BNE) ? 1 : 0;
 
-    //bne can't use and for aluop without checking for inverse of Zero
     $display("opcode=%b, jump=%b", opcode, jump);
   end
 endmodule
@@ -71,39 +104,39 @@ module jump_address_constructor(input [25:0] instruction, input [31:28] PC_plus_
     out = {PC_plus_4, (instruction<<2)};
 endmodule
 
-module test;
-  wire clock;
-  wire [31:0] adder_mux;
-  wire [31:0] mux_pc;
-  wire [31:0] instruction;
-  reg [31:0] four;
-  wire [31:0] address;
-  //reg [31:0] jump_address;
-  wire [31:0] jump_address;
-
-  wire [2:0] aluop;
-  wire regdst, jump, branch, memread, memtoreg, regwrite, alusrc, memwrite;
-
-
-  clock_gen clk(clock);
-  PC p_counter(mux_pc, clock, address);
-  adder pc_incrementer(address, four, adder_mux);
-  memory mem(address, instruction);
-  control control(instruction[31:26], regdst, jump, branch, memread, memtoreg, aluop, regwrite, alusrc, memwrite);
-  mux2_1 jump_mux(jump, jump_address, adder_mux, mux_pc);
-  jump_address_constructor jump_constructor(instruction[25:0], adder_mux[31:28], jump_address);
-
-  initial begin
-    four = 4;
-    //jump_address = 32'h00400000;
-    $dumpfile("jumps.vcd");
-    $dumpvars(0, test);
-    $monitor("instruction: %h\n", instruction);
-    #2000; $finish;
-  end
-  always @(instruction) begin
-    if (instruction == 0)
-      $finish;
-    else;
-  end
-endmodule
+// module test;
+//   wire clock;
+//   wire [31:0] adder_mux;
+//   wire [31:0] mux_pc;
+//   wire [31:0] instruction;
+//   reg [31:0] four;
+//   wire [31:0] address;
+//   //reg [31:0] jump_address;
+//   wire [31:0] jump_address;
+//
+//   wire [3:0] aluop;
+//   wire regdst, jump, branch, memread, memtoreg, regwrite, alusrc, memwrite, invertzero;
+//
+//
+//   clock_gen clk(clock);
+//   PC p_counter(mux_pc, clock, address);
+//   adder pc_incrementer(address, four, adder_mux);
+//   memory mem(address, instruction);
+//   control control(instruction[31:26], regdst, jump, branch, memread, memtoreg, aluop, regwrite, alusrc, memwrite, invertzero);
+//   mux2_1 jump_mux(jump, jump_address, adder_mux, mux_pc);
+//   jump_address_constructor jump_constructor(instruction[25:0], adder_mux[31:28], jump_address);
+//
+//   initial begin
+//     four = 4;
+//     //jump_address = 32'h00400000;
+//     $dumpfile("jumps.vcd");
+//     $dumpvars(0, test);
+//     $monitor("instruction: %h\n", instruction);
+//     #2000; $finish;
+//   end
+//   always @(instruction) begin
+//     if (instruction == 0)
+//       $finish;
+//     else;
+//   end
+// endmodule
