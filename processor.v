6828efee2b5f8509601b44152a7224d769896d0c
Daniel Vasquez <dtv002@bucknell.edu>
2017-09-10 11:57:40 -0400

    Added ALU module definitions

--- processor.v
+++ processor.v
@@ -97,11 +97,58 @@ module control (input [31:26] opcode, output reg regdst,jump,branch,memread,memt
 endmodule
 // -------------------------------------------------------
 
+// --------- ALU ------------ //
+module ALU (input [3:0] aluop, input [31:0] a, b, output [31:0] out);
+
+endmodule
+
+module ALU_control (input [3:0] aluop_from_control, input [5:0] functioncode, output [3:0] aluop_out);
+
+endmodule
+// -------------------------- //
+
 
 
 module jump_address_constructor(input [25:0] instruction, input [31:28] PC_plus_4, output reg [31:0] out);
   always @(*)
     out = {PC_plus_4, (instruction<<2)};
+endmodule
+
+module test;
+  wire clock;
+  wire [31:0] adder_mux;
+  wire [31:0] mux_pc;
+  wire [31:0] instruction;
+  reg [31:0] four;
+  wire [31:0] address;
+  //reg [31:0] jump_address;
+  wire [31:0] jump_address;
+
+  wire [3:0] aluop;
+  wire regdst, jump, branch, memread, memtoreg, regwrite, alusrc, memwrite, invertzero;
+
+
+  clock_gen clk(clock);
+  PC p_counter(mux_pc, clock, address);
+  adder pc_incrementer(address, four, adder_mux);
+  memory mem(address, instruction);
+  control control(instruction[31:26], regdst, jump, branch, memread, memtoreg, aluop, regwrite, alusrc, memwrite, invertzero);
+  mux2_1 jump_mux(jump, jump_address, adder_mux, mux_pc);
+  jump_address_constructor jump_constructor(instruction[25:0], adder_mux[31:28], jump_address);
+
+  initial begin
+    four = 4;
+    //jump_address = 32'h00400000;
+    $dumpfile("jumps.vcd");
+    $dumpvars(0, test);
+    $monitor("instruction: %h\n", instruction);
+    #2000; $finish;
+  end
+  always @(instruction) begin
+    if (instruction == 0)
+      $finish;
+    else;
+  end
 endmodule
 
 // module test;
