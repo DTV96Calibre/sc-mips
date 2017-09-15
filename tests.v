1ab802a6dcc84196666887d7a825e423cfcd1b9a
Daniel Vasquez <dtv002@bucknell.edu>
2017-09-15 18:25:40 -0400

    Wired up much of the processor

--- tests.v
+++ tests.v
@@ -1,33 +1,38 @@
 `include "processor.v"
 `include "mips.h"
 
-module test;
+module complete_processor;
   wire clock;
-  wire [31:0] adder_mux;
+  wire [31:0] pc_adder_mux, branch_adder_mux;
   wire [31:0] mux_pc;
   wire [31:0] instruction;
   reg [31:0] four;
   wire [31:0] address;
   //reg [31:0] jump_address;
-  wire [31:0] jump_address;
+  wire [31:0] jump_address, branch_address;
 
   wire [3:0] aluop;
-  wire regdst, jump, branch, memread, memtoreg, regwrite, alusrc, memwrite, invertzero;
-
+  wire regdst, jump, branch, memread, memtoreg, rtype, regwrite, alusrc, memwrite, invertzero;
+  wire branch_zero_and, branch_mux_control;
 
   clock_gen clk(clock);
   PC p_counter(mux_pc, clock, address);
-  adder pc_incrementer(address, four, adder_mux);
+  adder pc_incrementer(address, four, pc_adder_mux);
   instruction_memory imem(address, instruction);
-  control control(instruction[31:26], regdst, jump, branch, memread, memtoreg, aluop, regwrite, alusrc, memwrite, invertzero);
-  mux2_1 jump_mux(jump, jump_address, adder_mux, mux_pc);
-  jump_address_constructor jump_constructor(instruction[25:0], adder_mux[31:28], jump_address);
+  control control(instruction[31:26], regdst, jump, branch, memread, memtoreg, aluop, rtype, memwrite, alusrc, regwrite, invertzero);
+  mux32_2 jump_mux(jump_address, branch_address, jump, mux_pc);
+  jump_address_constructor jump_constructor(instruction[25:0], pc_adder_mux[31:28], jump_address);
+
+  and branch_zero_and(branch, zero, branch_zero_and);
+  inverter invertzero_inverter(branch_zero_and, invertzero, branch_mux_control);
+  mux32_2 branch_mux(branch_adder_mux, pc_adder_mux, branch_mux_control, branch_address);
+  adder branch_adder(pc_adder_mux, {16'h0, instruction[15:0]}<<2, branch_adder_mux);
 
   initial begin
     four = 4;
     //jump_address = 32'h00400000;
-    $dumpfile("jumps.vcd");
-    $dumpvars(0, test);
+    $dumpfile("processor.vcd");
+    $dumpvars(0, complete_processor);
     $monitor("instruction: %h\n", instruction);
     #2000; $finish;
   end
